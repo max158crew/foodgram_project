d4b434b564f76846f56f847d4917f8d826be4751
Maksim Sablin <maximsablin.spb@gmail.com>
2023-08-31 09:50:16 +0300

    Add ManyToMany relation for Recipe and Infredients

--- foodgram_backend/api/serializers.py
+++ foodgram_backend/api/serializers.py
@@ -1,9 +1,33 @@
 from rest_framework import serializers
 
-from recipes.models import Recipe
+from recipes.models import Recipe, Ingredient, IngredientRecipe
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'measure')
+
+
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True, required=False)
+
     class Meta:
         model = Recipe
-        fields = ('id', 'name', 'author', 'text')
+        fields = ('id', 'name', 'author', 'text', 'ingredients')
+
+
+    def create(self, validated_data):
+        if 'ingredients' not in self.initial_data:
+            recipe = Recipe.objects.create(**validated_data)
+            return recipe
+        ingredients = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)
+        for ingredient in ingredients:
+            current_ingredient, status = Ingredient.objects.get_or_create(**ingredient)
+            IngredientRecipe.objects.create(ingredient=current_ingredient, recipe=recipe)
+        return recipe
 
--- foodgram_backend/recipes/models.py
+++ foodgram_backend/recipes/models.py
@@ -3,16 +3,25 @@ from django.db import models
 class Tag(models.Model):
     pass
 
-class Ingridient(models.Model):
-    pass
+class Ingredient(models.Model):
+    name = models.CharField(max_length=128)
+    measure = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     name = models.CharField(max_length=128)
     author = models.CharField(max_length=16)
     text = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient, through='IngredientRecipe')
 
-class IngridientRecipe(models.Model):
-    pass
+    def __str__(self):
+        return self.name
+
+class IngredientRecipe(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
 class Cart(models.Model):
     pass
