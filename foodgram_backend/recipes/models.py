f23cf876ff20d061a9aea5b856511076155d3b68
Maksim Sablin <maximsablin.spb@gmail.com>
2023-08-31 09:27:02 +0300

    Add draft-models: Ingridient, Tag, Cart, Favorite

--- foodgram_backend/recipes/models.py
+++ foodgram_backend/recipes/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
+class Tag(models.Model):
+    pass
+
+class Ingridient(models.Model):
+    pass
 
 class Recipe(models.Model):
     name = models.CharField(max_length=128)
     author = models.CharField(max_length=16)
     text = models.TextField()
+
+class IngridientRecipe(models.Model):
+    pass
+
+class Cart(models.Model):
+    pass
+
+class Favorite(models.Model):
+    pass
 
