6a7033caca601088c2b7936c96bfa64c81df476d
Maksim Sablin <maximsablin.spb@gmail.com>
2023-09-19 09:36:17 +0300

    add management command Importcsv

---
+++ foodgram_backend/recipes/management/commands/importcsv.py
@@ -0,0 +1,36 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        self.stdout.write('seeding data...')
+        Command.run_seed()
+        self.stdout.write('done.')
+
+    @staticmethod
+    def clear_data():
+        """Deletes all the table data"""
+        print('Clear data started')
+        Ingredient.objects.all().delete()
+
+    @staticmethod
+    def run_seed():
+        Command.clear_data()
+        Command.seed_ingredients()
+
+    @staticmethod
+    def seed_ingredients():
+        print('Seed ingredients started')
+        with open('../data/ingredients.csv') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                print(row)
+                ingredient = Ingredient(name=row[0], measure=row[1])
+                ingredient.save()
+
