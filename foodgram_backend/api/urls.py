cd5779e9e9cf2cd93899f822c9090ed0cb8db1e9
Maksim Sablin <maximsablin.spb@gmail.com>
2023-10-08 23:46:00 +0300

    Add subscriptions

--- foodgram_backend/api/urls.py
+++ foodgram_backend/api/urls.py
@@ -1,18 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import RecipeViewSet, UserViewSet, TagViewSet, IngredientViewSet, ShowSubscriptionsView, SubscribeView
+from .views import RecipeViewSet, UsersViewSet, TagViewSet, IngredientViewSet
 
 router = DefaultRouter()
 router.register('recipes', RecipeViewSet)
-router.register('users', UserViewSet)
+router.register('users', UsersViewSet)
 router.register('ingredients', IngredientViewSet)
 router.register('tags', TagViewSet)
 
 
 urlpatterns = (
-    path('users/subscriptions/', ShowSubscriptionsView.as_view()),
-    path('users/suscribe', SubscribeView),
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 )
