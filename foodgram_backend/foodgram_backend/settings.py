4893bdde0a01d0387aa15decbd366236680d617a
Maksim Sablin <maximsablin.spb@gmail.com>
2023-10-13 13:56:14 +0300

    Deleted commetn code

--- foodgram_backend/foodgram_backend/settings.py
+++ foodgram_backend/foodgram_backend/settings.py
@@ -89,16 +89,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-#         "NAME": os.getenv("DB_NAME", default="postgres"),
-#         "USER": os.getenv("POSTGRES_USER", default="postgres"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="password"),
-#         "HOST": os.getenv("DB_HOST", default="db"),
-#         "PORT": os.getenv("DB_PORT", default="5432"),
-#     }
-# }
 
 
 # Password validation
